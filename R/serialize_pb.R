24c4a66a745f5164f04550cd4479aa1f949aab2a
Jeroen Ooms <jeroen.ooms@stat.ucla.edu>
2016-04-25 17:50:03 +0000

    version 1.1

--- R/serialize_pb.R
+++ R/serialize_pb.R
@@ -1,8 +1,9 @@
 #' Serialize to Protocol Buffers
 #'
 #' Serializes R objects to a general purpose protobuf message. It uses the same
-#' \code{rexp.proto} descriptor and mapping between R objects and protobuf messages
-#' as RHIPE and the \code{RProtoBuf} package.
+#' \href{https://github.com/jeroenooms/protolite/blob/master/src/rexp.proto}{rexp.proto}
+#' descriptor and mapping between R objects and protobuf messages as RHIPE and the
+#' \link[RProtoBuf:serialize_pb]{RProtoBuf} package.
 #'
 #' The \code{serialize_pb} and \code{unserialize_pb} reimplement the identically
 #' named functions from the \code{RProtoBuf} package in pure \code{C++}. This makes
@@ -15,6 +16,9 @@
 #' @aliases protolite
 #' @param object an R object to serialize
 #' @param connection a connection, file, or \code{NULL} for a raw vector
+#' @param skip_native do not serialize 'native' (non-data) R objects. Setting to \code{TRUE}
+#' will only serialize \emph{data} types (numeric, boolean, string, raw, list). The default
+#' behavior is to fall back on base R \code{\link{serialize}} for non-data objects.
 #' @param msg raw vector with the serialized \code{rexp.proto} message
 #' @examples # Serialize and unserialize an object
 #' buf <- serialize_pb(iris)
@@ -31,8 +35,9 @@
 #' out <- RProtoBuf::unserialize_pb(buf)
 #' stopifnot(identical(mtcars, out))
 #' }
-serialize_pb <- function(object, connection = NULL){
-  buf <- cpp_serialize_pb(object)
+serialize_pb <- function(object, connection = NULL, skip_native = FALSE){
+  stopifnot(is.logical(skip_native))
+  buf <- cpp_serialize_pb(object, skip_native)
   if(is.null(connection))
     return(buf)
   writeBin(buf, con = connection)
